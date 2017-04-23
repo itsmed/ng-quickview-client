2f9af8eb396f81d9d2b902505c55a03a63cc47ad
DejonGill <dejonjgill@gmail.com>
2017-04-23 16:24:30 -0700

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  AngularFire,
+  FirebaseListObservable,
+} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  items: FirebaseListObservable<any []>;
+
+  constructor(
+    private af: AngularFire,
+  ) {}
+
+  ngOnInit() {
+    this.items = this.af.database.list('/items');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAFSVxEdT1QIZYgKJUpKD_J5vtRE7l4msY",
+  authDomain: "superhero-booking-agency.firebaseapp.com",
+  databaseURL: "https://superhero-booking-agency.firebaseio.com",
+  projectId: "superhero-booking-agency",
+  storageBucket: "superhero-booking-agency.appspot.com",
+  messagingSenderId: "437210707211"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
